8c23b8a05fcfa0b9a7c5d49fde657cb13cef4721
santvg98 <139291797+santvg98@users.noreply.github.com>
2023-10-31 22:48:36 -0500

    Se añade archivo "js" y se crea codigo "css

---
+++ script.js
@@ -0,0 +1,17 @@
+function sumar() 
+{
+            
+    var num1 = parseFloat(document.getElementById("numero1").value);
+    var num2 = parseFloat(document.getElementById("numero2").value);
+
+  
+    if (!isNaN(num1) && !isNaN(num2)) 
+    {
+        var suma = num1 + num2;
+        document.getElementById("resultado").innerHTML = suma;
+    } 
+    else 
+    {
+        document.getElementById("resultado").innerHTML = "Por favor, ingresa números válidos.";
+    }
+}
