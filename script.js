24ada85a6bd959935116bdbbcd482b02a241d9ce
Nuevo_usuario <Nuevo_usuario.com>
2023-11-08 08:19:27 -0500

    se añaden las demas operaciones

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-function sumar() 
-{
-            
+function calcular() {
     var num1 = parseFloat(document.getElementById("numero1").value);
     var num2 = parseFloat(document.getElementById("numero2").value);
+    var operacion = document.getElementById("operacion").value;
 
-  
-    if (!isNaN(num1) && !isNaN(num2)) 
-    {
-        var suma = num1 / num2;
-        document.getElementById("resultado").innerHTML = suma;
-    } 
-    else 
-    {
+    if (!isNaN(num1) && !isNaN(num2)) {
+        var resultado;
+        switch (operacion) {
+            case "suma":
+                resultado = num1 + num2;
+                break;
+            case "resta":
+                resultado = num1 - num2;
+                break;
+            case "multiplicacion":
+                resultado = num1 * num2;
+                break;
+            case "division":
+                if (num2 !== 0) {
+                    resultado = num1 / num2;
+                } else {
+                    document.getElementById("resultado").innerHTML = "No se puede dividir por cero.";
+                    return;
+                }
+                break;
+        }
+        document.getElementById("resultado").innerHTML = resultado;
+    } else {
         document.getElementById("resultado").innerHTML = "Por favor, ingresa números válidos.";
     }
 }
